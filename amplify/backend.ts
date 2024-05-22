4a1c063e67d0a31062ecc1537eb1ae0ac91d0607
Fazt <fazt@faztweb.com>
2024-05-22 10:05:14 -0500

    some auth setup added

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function App() {
     <Authenticator>
       {({ signOut, user }) => (
         <main>
-          <h1>My todos</h1>
+          <h1>{user?.signInDetails?.loginId}'s todos</h1>
           <button onClick={createTodo}>+ new</button>
           <ul>
             {todos.map((todo) => (
@@ -58,6 +58,7 @@ export default function App() {
               Review next steps of this tutorial.
             </a>
           </div>
+          <button onClick={signOut}>Sign out</button>
         </main>
       )}
     </Authenticator>
